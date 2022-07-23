ea393c1f606f16d9c02ba3e0aababe07f39c375b
cuixuechao <876920906@qq.com>
2022-07-23 17:29:47 +0800

    更新部分注释

--- src/main/java/singletonPattern/HungrySingleton.java
+++ src/main/java/singletonPattern/HungrySingleton.java
@@ -1,10 +1,10 @@
 package singletonPattern;
 
 /**
- * @ClassName: HungryMan
- * @DESCRIPTION: 单例模式，饿汉式,基于jvm的类加载机制实现线程安全
- * @author: cxc
- * @date: 2021/3/15
+ * 饿汉式,基于jvm的类加载机制实现,线程安全
+ *
+ * @author cxc
+ * @date 2022/07/23  16:59
  */
 
 public class HungrySingleton {
